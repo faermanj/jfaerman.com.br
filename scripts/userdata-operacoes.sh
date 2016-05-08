c1b94130701f5a4df317307916aa478cf60ce312
Julio Faerman <jfaerman@gmail.com>
2016-05-08 16:31:52 -0300

    Initial Jekyll Website

--- scripts/userdata-operacoes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yum -y install httpd php
-service httpd start
-chkconfig httpd on
-echo "Oi Entrega Continua" > /var/www/html/index.html
--- userdata-website.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yum -y install httpd php git
-service httpd start
-chkconfig httpd on
-git clone https://github.com/jfaerman/jfaerman.com.br.git /var/www/html
